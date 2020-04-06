7b590d4751ddc8068b2887bc9d1b5fc4ac57b34e
Adrian Li <li.adrianmc@gmail.com>
2020-04-06 02:39:01 -0400

    add segment

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,5 +79,51 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-segment-js`,
+      options: {
+        // your segment write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        prodKey: process.env.SEGMENT_API_KEY,
+
+        // if you have a development env for your segment account, paste that key here
+        // when process.env.NODE_ENV === 'development'
+        // optional; non-empty string
+        devKey: process.env.SEGMENT_API_KEY,
+
+        // boolean (defaults to false) on whether you want
+        // to include analytics.page() automatically
+        // if false, see below on how to track pageviews manually
+        trackPage: true,
+
+        // If you need to proxy events through a custom endpoint,
+        // add a `host` property (defaults to https://cdn.segment.io)
+        // Segment docs:
+        //   - https://segment.com/docs/guides/sources/custom-domains
+        //   - https://segment.com/docs/sources/website/analytics.js/#proxy
+        host: `https://override-segment-endpoint`,
+
+        // boolean (defaults to false); whether to delay load Segment
+        // ADVANCED FEATURE: only use if you leverage client-side routing (ie, Gatsby <Link>)
+        // This feature will force Segment to load _after_ either a page routing change
+        // or user scroll, whichever comes first. This delay time is controlled by
+        // `delayLoadTime` setting. This feature is used to help improve your website's
+        // TTI (for SEO, UX, etc).  See links below for more info.
+        // NOTE: But if you are using server-side routing and enable this feature,
+        // Segment will never load (because although client-side routing does not do
+        // a full page refresh, server-side routing does, thereby preventing Segment
+        // from ever loading).
+        // See here for more context:
+        // GIF: https://github.com/benjaminhoffman/gatsby-plugin-segment-js/pull/19#issuecomment-559569483
+        // TTI: https://github.com/GoogleChrome/lighthouse/blob/master/docs/scoring.md#performance
+        // Problem/solution: https://marketingexamples.com/seo/performance
+        delayLoad: false,
+
+        // number (default to 1000); time to wait after scroll or route change
+        // To be used when `delayLoad` is set to `true`
+        delayLoadTime: 1000,
+      },
+    },
   ],
 }
